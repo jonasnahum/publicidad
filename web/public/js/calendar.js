e71a35fb8653305d2151d014e3f028ed3adfbac5
Mon se <monserrat.jimenez2@gmail.com>
2015-11-12 07:46:16 -0600

    trabajando datepicker, elegir un rango de fechas

--- web/public/js/calendar.js
+++ web/public/js/calendar.js
@@ -1,7 +1,28 @@
 $(function() {
+    $( "#fechaContrato" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        onClose: function( selectedDate ) {
+            $("#fechaVncimiento").datepicker("option", "minDate", selectedDate);
+        }
+    });
+    $( "#fechaVencimiento" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        onClose: function( selectedDate ) {
+            $( "#fechaContrato" ).datepicker ( "option", "maxDate", selectedDate );       
+        }
+    });
+    
+});
+
+/*
+$(function() {
     $( "#datepicker" ).datepicker();
 });
 
 $(function() {
     $( "#datepicker2" ).datepicker();
 });
+*/
+
