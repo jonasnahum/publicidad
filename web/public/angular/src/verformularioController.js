e0a8a551b9cea68419ce30813cbd48012943e864
Mon se <monserrat.jimenez2@gmail.com>
2015-11-13 12:34:15 -0600

    Undefined al mostrar mapa

--- web/public/angular/src/verformularioController.js
+++ web/public/angular/src/verformularioController.js
@@ -7,19 +7,24 @@
         ctrl.empresaId= "5645e902713f97c11f50d3aa";
         ctrl.direccionId = "5645e906713f97c11f50d3ab";
         ctrl.rubroId = "5645e906713f97c11f50d3ac";
-        ctrl.informacionId = "5645e906713f97c11f50d3ad";        
+        ctrl.informacionId = "5645e906713f97c11f50d3ad";  
+        
+        ctrl.lat = undefined;  
+        ctrl.long = undefined;  
         
         ctrl.verEmpresa = function() {
             $http({
                 url: 'http://localhost:3000/empresas/api/' + ctrl.empresaId,
                 method: "GET",
             }).success(function(data, status, headers, config){
+                console.log(data.lat);
+                console.log(data.long);
                 ctrl.nombre = data.nombre;
-                //ctrl.logotipo = undefined;
-                //ctrl.foto = undefined;
+                ctrl.logotipo = data.logotipo;
+                ctrl.foto = data.foto;
                 ctrl.textoIntro = data.textoIntro;
-                //ctrl.lat = data.lat;
-                //ctrl.long = data.long;
+                ctrl.lat = data.lat;
+                ctrl.long = data.long;
                 ctrl.descripcion = data.descripcion;
                 ctrl.horario = data.horario;
                 ctrl.encargado = data.encargado;
@@ -82,7 +87,36 @@
         ctrl.verEmpresa();
         ctrl.verDireccion();
         ctrl.verRubro();
-        ctrl.verInformacion();
+        ctrl.verInformacion();    
+        
+        console.log(ctrl.lat);//aparecen como undefined
+        console.log(ctrl.long);
+        
+        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+        ctrl.latitud = ctrl.lat;
+        ctrl.longitud = ctrl.long;
+        var myMarkerPosition=new google.maps.LatLng(ctrl.latitud,ctrl.longitud);
+       
+        var map;
+        var myCenter=new google.maps.LatLng(ctrl.lat,ctrl.long);
+
+        function initialize(){
+            var mapOptions = {
+                  center:myCenter,
+                  zoom:13,
+                  mapTypeId:google.maps.MapTypeId.ROADMAP
+              };
+            map = new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+            var marker = new google.maps.Marker({
+                position: myMarkerPosition,
+                map: map,
+                icon: iconBase + 'schools_maps.png'
+            });
+        }
+               
+        
+        google.maps.event.addDomListener(window, 'load', initialize);
+        
     }]);
 })();
 
