0fcee15580fc5344936a9b382fdf30565aef6904
jonasnahum <jonasnahum@gmail.com>
2015-11-16 16:10:50 -0600

    starting to validate

--- server/src/empresaCollection.js
+++ server/src/empresaCollection.js
@@ -1,6 +1,12 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+var minFive = function(val) {
+    if (val && val.length >= 5 ){
+        return true;
+    }
+    return false;
+};
 var minTen = function(val) {
     if (val && val.length >= 10 ){
         return true;
@@ -13,18 +19,54 @@ var maxForty = function(val) {
     }
     return false;
 };
+var maxEighty = function(val) {
+    if (val && val.length <= 80 ){
+        return true;
+    }
+    return false;
+};
+var maxFiveTeen = function(val) {
+    if (val && val.length <= 15 ){
+        return true;
+    }
+    return false;
+};
+var  maxOeighty = function(val) {
+    if (val && val.length <= 180 ){
+        return true;
+    }
+    return false;
+};
+var  maxO = function(val) {
+    if (val && val.length <= 100 ){
+        return true;
+    }
+    return false;
+};
 
 var letrasEspacios = {validator: /^[A-Za-z ]+$/, msg: 'Este campo sólo acepta letras y espacios'};
-var numeros = [
-    {validator: /^[0-9]*$/, msg: 'Este campo sólo acepta letras y espacios'}
-];
+var numeros = {validator: /^[0-9]*$/, msg: 'Este campo sólo acepta letras y espacios'};
 
 var validateNombre = [
     {validator: minTen, msg: 'Demasiado corto'} ,
     {validator: maxForty, msg: 'Demasiado largo'} ,
     letrasEspacios
 ];
-
+var validateTextoIntro = [
+    {validator: maxEighty, msg: 'Demasiado largo'} ,
+];
+var validateDescripcion = [
+    letrasEspacios,
+    {validator: maxOeighty, msg: 'Demasiado largo'} ,
+];
+var validateHorario = [
+    {validator: maxO, msg: 'Demasiado largo'} ,
+];
+var validateTel = [
+    {validator: minFive, msg: 'Demasiado corto'} ,
+    {validator: maxFiveTeen, msg: 'largo corto'},
+    numeros,
+];
 var direccionSchema = mongoose.Schema({
     numero: String,
     numeroInt: String,
@@ -54,13 +96,13 @@ var schema = mongoose.Schema({
     nombre: { type: String, required: true, validate: validateNombre },
     logotipo: String, 
     foto: String, 
-    textoIntro: String, 
+    textoIntro: { type: String, required: true, validate: validateTextoIntro }, 
     lat: String, 
     long: String, 
-    descripcion: String, 
-    horario: String, 
-    encargado: String, 
-    tel: String, 
+    descripcion: { type: String, validate: validateDescripcion }, 
+    horario: { type: String, required: true, validate: validateHorario }, 
+    encargado: { type: String, required: true, validate: validateNombre }, 
+    tel: { type: String, required: true, validate: validateTel },
     face: String, 
     email: String, 
     productos: [],
