fac88a19ee311f9cdf89a6b78115ace8fb83809e
Mon se <monserrat.jimenez2@gmail.com>
2015-11-17 10:09:53 -0600

    Mapa se muestra

--- web/public/angular/src/verformularioController.js
+++ web/public/angular/src/verformularioController.js
@@ -15,8 +15,8 @@
                 ctrl.logotipo = data.logotipo;
                 ctrl.foto = data.foto;
                 ctrl.textoIntro = data.textoIntro;
-                ctrl.lat = 19.404171438833234;
-                ctrl.long = -102.06431865692139;
+                ctrl.lat = data.lat;
+                ctrl.long = data.long;
                 ctrl.descripcion = data.descripcion;
                 ctrl.horario = data.horario;
                 ctrl.encargado = data.encargado;
@@ -80,69 +80,5 @@
 
 //            google.maps.event.addDomListener(window, 'load', initialize);
 
-
-        
-/*        //MAPA NO SE MUESTRA
-        promise1().then(
-            function() {
-                console.log('SE EJECUTO mapa');
-                
-                var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-                var latitud = parseFloat(ctrl.lat);
-                var longitud = parseFloat(ctrl.long);
-                var myMarkerPosition=new google.maps.LatLng(latitud,longitud);
-
-                console.log(latitud);//aparecen como undefined
-                console.log(longitud);
-
-                var map;
-                var myCenter=new google.maps.LatLng(latitud,longitud);
-
-                function initialize(){
-                    var mapOptions = {
-                          center:myCenter,
-                          zoom:13,
-                          mapTypeId:google.maps.MapTypeId.ROADMAP
-                      };
-                    map = new google.maps.Map(document.getElementById("googleMap"),mapOptions);
-                    var marker = new google.maps.Marker({
-                        position: myMarkerPosition,
-                        map: map,
-                        icon: iconBase + 'schools_maps.png'
-                    });
-                }
-
-                google.maps.event.addDomListener(window, 'load', initialize);
-            });
-    */    
-        //CODIGO DEL EMAIL 
-        
-       /* promise1();
-        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
-        ctrl.latitud = 19.4077;
-        ctrl.longitud = -102.0642;
-        var myMarkerPosition=new google.maps.LatLng(ctrl.latitud,ctrl.longitud);
-       
-        var map;
-        var myCenter=new google.maps.LatLng(19.4096,-102.0520);
-
-        function initialize(){
-            var mapOptions = {
-                  center:myCenter,
-                  zoom:13,
-                  mapTypeId:google.maps.MapTypeId.ROADMAP
-            };
-            map = new google.maps.Map(document.getElementById("googleMap"),mapOptions);
-            var marker = new google.maps.Marker({
-                position: myMarkerPosition,
-                map: map,
-                icon: iconBase + 'schools_maps.png'
-            });
-        }
-               
-        
-        google.maps.event.addDomListener(window, 'load', initialize);
-       */ 
-        
     }]);
 })();
