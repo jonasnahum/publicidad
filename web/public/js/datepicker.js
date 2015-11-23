1304c21023c4a0f28ae6c82c42c14b60d5d7d8ae
Mon se <monserrat.jimenez2@gmail.com>
2015-11-23 11:10:50 -0600

    cambio en la validacion de nuevo.html

--- web/public/js/datepicker.js
+++ web/public/js/datepicker.js
@@ -4,25 +4,16 @@ $(function() {
         changeMonth: true,
         onClose: function( selectedDate ) {
             $("#fechaVncimiento").datepicker("option", "minDate", selectedDate);
-        }
+        },
+        dateFormat: "yy-mm-dd"
     });
     $( "#fechaVencimiento" ).datepicker({
         defaultDate: "+1w",
         changeMonth: true,
         onClose: function( selectedDate ) {
             $( "#fechaContrato" ).datepicker ( "option", "maxDate", selectedDate );       
-        }
+        },
+        dateFormat: "yy-mm-dd"
     });
     
 });
-
-/*
-$(function() {
-    $( "#datepicker" ).datepicker();
-});
-
-$(function() {
-    $( "#datepicker2" ).datepicker();
-});
-*/
-
