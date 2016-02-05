f6323f5090dd9cf180c38aaba754a917b1a87890
rodrigo <jonasnahum@gmail.com>
2016-02-05 11:23:41 -0600

    hedear acepta publicas y privadas

--- web/public/angular/src/routeChecker.js
+++ web/public/angular/src/routeChecker.js
@@ -10,18 +10,11 @@
             this.url = $location.path();
             
             for (i = 0; i < this.privatePages.length ; i++){
-                if(this.url.indexOf(i) > -1)
+                var str = this.privatePages[i];
+                if(this.url.indexOf(str) > -1)
                     return true;
             }
             return false;
-
-        };
-        RouteChecker.prototype.addFreePage = function (page) {
-            var casilla = this.url.indexOf(page);
-            if(casilla == 0){
-               this.freeAccesPages.push(this.url);
-            };    
-            return
         };
         RouteChecker.prototype.isActive = function (path) {
             return path === $location.path();
