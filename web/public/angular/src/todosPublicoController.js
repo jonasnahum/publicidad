338002684d439497cb200eabc64726520e466150
Monse <monserrat.jimenez2@gmail.com>
2016-02-09 10:03:28 -0600

    Bug eliminado, al dar click ya muestra la empresa

--- web/public/angular/src/todosPublicoController.js
+++ web/public/angular/src/todosPublicoController.js
@@ -4,11 +4,10 @@
         var ctrl = this;
         ctrl.empresas = [];
         ctrl.searchText = "";
-        ctrl.requestedPageNumber;
-        ctrl.myPageItemsCount = 4;
+        //ctrl.requestedPageNumber;
+        //ctrl.myPageItemsCount = 4;
 
         ctrl.getAll = function(){
-            console.log(ctrl.requestedPageNumber);
             empresasProxy.getAll(function(data){
                 ctrl.empresas=data;
             });
