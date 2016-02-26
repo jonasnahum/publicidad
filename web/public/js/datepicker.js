f53c4727de8f5c115c57b2da0b9cca4d197e6f11
Mongit <monserrat.jimenez2@gmail.com>
2016-02-26 13:29:02 -0600

    Se eliminaron comentarios

--- web/public/js/datepicker.js
+++ web/public/js/datepicker.js
@@ -1,32 +1,7 @@
-/*$(function() {
-    $( "#fechaContrato" ).datepicker({
-        defaultDate: "+1w",
-        changeMonth: true,
-        onClose: function( selectedDate ) {
-            $("#fechaVencimiento").datepicker("option", "minDate", selectedDate);
-        },
-        dateFormat: "yy-mm-dd"
-    });
-    $( "#fechaVencimiento" ).datepicker({
-        defaultDate: "+1w",
-        changeMonth: true,
-        onClose: function( selectedDate ) {
-            $( "#fechaContrato" ).datepicker ( "option", "maxDate", selectedDate );       
-        },
-        dateFormat: "yy-mm-dd"
-    });
-    
-});
-*/
-
 $(function() {
     $( "#fechaContrato" ).datepicker({
         defaultDate: "+1w",
         changeMonth: true,
-        //onClose: function( selectedDate ) {
-          //  $("#fechaVencimiento").datepicker("option", "minDate", selectedDate);
-        //},
-        //dateFormat: "yy-mm-dd"
     }); 
 });
 
@@ -34,9 +9,5 @@ $(function() {
    $( "#fechaVencimiento" ).datepicker({
         defaultDate: "+1w",
         changeMonth: true,
-        //onClose: function( selectedDate ) {
-          //  $( "#fechaContrato" ).datepicker ( "option", "maxDate", selectedDate );       
-        //},
-        //dateFormat: "yy-mm-dd"
     });
 });
