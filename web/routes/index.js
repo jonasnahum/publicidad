ab140c64295685efb574ec09ccf71a5a53b0db29
Mongit <monserrat.jimenez2@gmail.com>
2016-03-11 09:18:20 -0600

    pejemplo portada grayscale se ve correctamente

--- web/routes/index.js
+++ web/routes/index.js
@@ -5,7 +5,11 @@ var serverInfo = { server: process.env.SERVER || 'http://localhost:3000' };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Web Uruapan', serverInfo: serverInfo });
+    //res.render('index', { title: 'Web Uruapan', serverInfo: serverInfo });
+    res.render('index2', { title: 'Web Uruapan', serverInfo: serverInfo });
+});
+router.get('/prueba', function(req, res, next) {
+    res.render('prueba', { title: 'Web Uruapan', serverInfo: serverInfo });
 });
 
 module.exports = router;
