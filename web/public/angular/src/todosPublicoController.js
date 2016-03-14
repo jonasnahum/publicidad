641d75d8cff2df0087cef859542815211a783f1c
Mongit <monserrat.jimenez2@gmail.com>
2016-03-14 08:40:46 -0600

    se cambio la presentacion por el contenido de paginas web

--- web/public/angular/src/todosPublicoController.js
+++ web/public/angular/src/todosPublicoController.js
@@ -1,12 +1,19 @@
 (function() {
     var app = angular.module('app');
-    app.controller('TodosPublicoController', ['paginasProxy', function(paginasProxy) {
+    app.controller('TodosPublicoController', ['paginasProxy', '$anchorScroll', '$location', function(paginasProxy, $anchorScroll, $location) {
+        
         var ctrl = this;
         ctrl.paginas = [];
         ctrl.searchText = "";
         //ctrl.requestedPageNumber;
         //ctrl.myPageItemsCount = 4;
 
+        ctrl.scrollTo = function(id) {
+           $location.hash(id);
+           $anchorScroll();
+        };
+        
+        
         ctrl.getAll = function(){
             paginasProxy.getAll(function(data){
                 ctrl.paginas=data;
