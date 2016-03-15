2e0eb0249191f3de968917aaa0dc5de9c1091351
Mongit <monserrat.jimenez2@gmail.com>
2016-03-15 10:25:00 -0600

    Todospub.html, nueva presentacion

--- web/public/angular/src/todosPublicoController.js
+++ web/public/angular/src/todosPublicoController.js
@@ -1,6 +1,6 @@
 (function() {
     var app = angular.module('app');
-    app.controller('TodosPublicoController', ['paginasProxy', '$anchorScroll', '$location', function(paginasProxy, $anchorScroll, $location) {
+    app.controller('TodosPublicoController', ['paginasProxy', '$anchorScroll', '$location', 'mapFactory', function(paginasProxy, $anchorScroll, $location, mapFactory) {
         
         var ctrl = this;
         ctrl.paginas = [];
@@ -17,6 +17,11 @@
         ctrl.getAll = function(){
             paginasProxy.getAll(function(data){
                 ctrl.paginas=data;
+                
+                var latitud = parseFloat(19.412288699925995);
+                var longitud = parseFloat(-102.0718502998352);
+                var mapa = mapFactory(latitud,longitud);
+                mapa.placeMarker(latitud,longitud);
             });
         };
         ctrl.getAll();  
