f7cfe18b60049295f93653d61cabceba2d90cfee
jonasnahum <jonasnahum@gmail.com>
2016-03-30 15:41:52 -0600

    delete changed

--- server/src/paginaWebApi.js
+++ server/src/paginaWebApi.js
@@ -80,7 +80,7 @@ var PaginaWebApi = (function() {
        //borra el usuario.
        that.models.usuario.findByIdAndRemove({ _id: req.params.id }, function(err, user) {
            if(err) return next(err);
-           that.models.paginaWeb.findByIdAndRemove({_usuario: user.id}, function(err, pag) {
+           that.models.paginaWeb.remove({_usuario: user.id}, function(err, pag) {
                if(err) return next(err);
                res.json(pag);
            });
