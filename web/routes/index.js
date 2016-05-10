871ae201d6e8ce7446b1d88e953229f0c1605c40
jonasnahum <jonasnahum@gmail.com>
2016-05-10 12:17:02 -0500

    funciona y no muestra error en jquery

--- web/routes/index.js
+++ web/routes/index.js
@@ -5,9 +5,7 @@ var serverInfo = { server: process.env.SERVER || 'http://localhost:3000' };
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Web Uruapan', serverInfo: serverInfo }, function(err, html){
-        res.send(html);
-    });
+    res.render('index', { title: 'Web Uruapan', serverInfo: serverInfo });           
 });
 
 module.exports = router;
